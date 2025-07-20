d240fc97eb1d06a543702624c31e0d25725c15e1
Jonathan Linder <linderjonathan3@gmail.com>
2025-07-20 19:52:24 +0200

    set up classes

---
+++ src/hasher.cpp
@@ -0,0 +1,13 @@
+#include <hasher.hpp>
+#include <fstream>
+#include <string>
+#include <iostream>
+
+#include <openssl/sha.h>
+
+std::string Hasher::hash(const std::string &filePath) {
+
+    // TODO: open file
+    
+    return "";
+}
