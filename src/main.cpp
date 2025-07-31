d3a5d79d04fb5ad147583e376ac0f3925cf5a9e2
Jonathan Linder <linderjonathan3@gmail.com>
2025-07-31 23:18:09 +0200

    Horrible push (adding public key save)

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,32 @@
 #include <iostream>
 #include <string>
 #include "hasher.hpp"
+#include "keyHandler.hpp"
 int main () {
 
-    Hasher h;
 
-    std::string hash = h.hash("test/helloWorld.txt");
-    std::cout << hash << std::endl;
+
+    // TODO: generate private and public key
+
+    std::string fp;
+    std::string pwd;
+    std::cin >> fp;
+    std::cin >> pwd;
+    std::string error;
+    KeyHandler kh;
+
+    error = kh.generateKeyPair();
+    if (!error.empty()) {
+        // write error to GUI, write to terminal for now
+        std::cout << error << std::endl;
+        return 0;
+    }
+
+    error = kh.storeKeyPair(pwd, "../test_pkcs8/test.pem");
+    if (!error.empty()) {
+        // write error to GUI, write to terminal for now
+        std::cout << error << std::endl;
+        return 0;
+    }
     return 0;
 }
