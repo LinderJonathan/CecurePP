1a844c557bc01180eb8b1995fb70e0a1925a27fc
Jonathan Linder <linderjonathan3@gmail.com>
2025-09-13 09:07:04 +0200

    First rendered rectangle!

--- src/graphics/windowHandler.cpp
+++ src/graphics/windowHandler.cpp
@@ -1,5 +1,11 @@
 #include "windowHandler.hpp"
 
+/**
+ * @file    windowHandler.cpp
+ * @brief   Handles main event loop window routing
+ * 
+ */
+
 windowHandler::windowHandler() : window(NULL), renderer(NULL) {
     SDL_Init;
 
@@ -12,29 +18,36 @@ windowHandler::windowHandler() : window(NULL), renderer(NULL) {
         WINDOW_HEIGHT,
         WINDOW_FLAGS_DEFAULT
     );
-    // TODO: handle SDL_Createrenderer return NULL case
+    if (!window) {
+        std::cout << "Unnable to create window: " << SDL_GetError() << "\n";
+        SDL_Quit();
+    }
 
     renderer = SDL_CreateRenderer(window, -1,  RENDERER_FLAGS_DEFAULT);
+    if (!renderer) {
+        std::cout << "Unnable to create renderer: " << SDL_GetError() << "\n";
+        SDL_DestroyWindow(window);
+        SDL_Quit();
+
+    }
     running = true;
 }
 
-windowHandler::~windowHandler() {
-    close();
-}
+windowHandler::~windowHandler() { close(); }
+bool windowHandler::getRunningState() { return running; }
+void windowHandler::setRunningState(bool state) { running = state; }
 
-bool windowHandler::getRunningState() {
-    return running;
-}
+void windowHandler::renderStartScreen() {
 
-void windowHandler::setRunningState(bool state) {
-    running = state;
-}
-void windowHandler::drawWindow() {
-    
+    const SDL_Rect quitButton = {200, 200, 200, 200};
+
+    SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+    SDL_RenderDrawRect(renderer, &quitButton);
+    SDL_RenderFillRect(renderer, &quitButton);
+    SDL_RenderPresent(renderer);
 }
 
 void windowHandler::close() {
-    // TODO: de-allocate some window structs???
     SDL_DestroyWindow(window);
     SDL_DestroyRenderer(renderer);
     SDL_Quit();
