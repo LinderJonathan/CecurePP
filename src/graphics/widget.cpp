6aeb7d519bb8348aa4bac7896a9cd09c7df3efc3
LinderJonathan <linderjonathan3@gmail.com>
2025-09-21 19:57:13 +0200

    added defines for font size. font size part of input

--- src/graphics/widget.cpp
+++ src/graphics/widget.cpp
@@ -5,13 +5,15 @@ button::button(
     int y,
     int width,
     int height,
+    int fontSize,
     const char *lbl,
     std::function<void()> cb,
     buttonTheme theme
 ) : 
+    rect{x, y, width, height},
+    font(TTF_OpenFont(FONT_PATH_1, fontSize)),
     label(lbl), 
     callback(cb),
-    rect{x, y, width, height},
     theme(theme){
 
 }
@@ -23,13 +25,12 @@ button::~button(){
 void button::render(SDL_Renderer *renderer, buttonTheme theme, TTF_Font *font) {
 
     // Render outline
-    
     SDL_SetRenderDrawColor(
         renderer,
         theme.outlineColor.r,
         theme.outlineColor.g,
         theme.outlineColor.b,
-        theme.outlineColor.alpha
+        theme.outlineColor.a
     );
     SDL_RenderDrawRect(renderer, &rect);
 
@@ -39,20 +40,23 @@ void button::render(SDL_Renderer *renderer, buttonTheme theme, TTF_Font *font) {
         theme.bgColor.r,
         theme.bgColor.g,
         theme.bgColor.b,
-        theme.bgColor.alpha
+        theme.bgColor.a
     );
     SDL_RenderFillRect(renderer, &rect);
     
-    SDL_Rect Message_rect; //create a rect
-    Message_rect.x = 0;  //controls the rect's x coordinate 
-    Message_rect.y = 0; // controls the rect's y coordinte
-    Message_rect.w = 100; // controls the width of the rect
-    Message_rect.h = 100; // controls the height of the rect
-    // TODO: check stack overflow page
+    // Render surface from 
     SDL_Surface *surfaceLabel = TTF_RenderText_Solid(font, label, theme.textColor);
+    SDL_Rect Message_rect;
+    Message_rect.x = rect.x + (rect.w - surfaceLabel->w) / 2; 
+    Message_rect.y = rect.y + (rect.h - surfaceLabel->h) / 2;
+    Message_rect.h = surfaceLabel->h;
+    Message_rect.w = surfaceLabel->w;
     SDL_Texture *textureLabel = SDL_CreateTextureFromSurface(renderer, surfaceLabel);
-    SDL_RenderCopy(renderer, textureLabel, NULL, &rect);
+    
+    SDL_RenderCopy(renderer, textureLabel, NULL, &Message_rect);
     SDL_RenderPresent(renderer);
 }
 
-void handleEvent() {}
+void button::handleEvent(SDL_Event *event) {
+    callback();
+}
